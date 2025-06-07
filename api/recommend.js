9cfb68ef22b6b7dcd1f5716cbc0c5eaedc9d266d
sndrrt <sanderrietdijk@gmail.com>
2025-06-07 14:23:16 +0200

    Update recommend.js

--- api/recommend.js
+++ api/recommend.js
@@ -1,6 +1,8 @@
 export default async function handler(req) {
   return new Response(
-    JSON.stringify({ message: "✅ verse project function werkt!" }),
-    { headers: { "Content-Type": "application/json" } }
+    JSON.stringify({ message: "✅ werkt" }),
+    {
+      headers: { "Content-Type": "application/json" }
+    }
   );
 }
